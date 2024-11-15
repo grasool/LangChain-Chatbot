f0b03eaf7e0bf1fca5455594d3fb63dd924884a6
P82maarj <55245863+P82maarj@users.noreply.github.com>
2024-11-15 23:47:44 +0100

    Update RAG-chatbot.py

--- RAG-chatbot.py
+++ RAG-chatbot.py
@@ -2,6 +2,8 @@
 #
 #from openai import OpenAI
 import asyncio
+import os
+import time
 from openai import AsyncOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -11,6 +13,7 @@ from langchain.chains import LLMChain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
+
 
 template_1 = """
     Act as a techinal expert. Answer the question based only on the following context:
@@ -23,17 +26,21 @@ prompt = ChatPromptTemplate.from_template(template_1)
 
 @st.cache_resource
 def prepareDB():
-    markdown_path = "./knowledgebase/"
-    markdown_loader = DirectoryLoader(markdown_path, glob='./*.md', loader_cls=UnstructuredMarkdownLoader)
-    markdown_docs = markdown_loader.load()
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=30)
-    docs = text_splitter.split_documents(markdown_docs)
-
-    # Generate the embeddings
     model="sentence-transformers/all-MiniLM-L12-v2"
     embeddings = HuggingFaceEmbeddings(model_name=model)
-    vector_db= Chroma.from_documents(documents=docs,embedding=embeddings,persist_directory="./chroma_db")
+
+    db_Exist=os.path.isfile("./chroma_db/36ed155a-48e2-4c4c-a9c7-1cc3ce565724/data_level0.bin")
+    if db_Exist==False:
+        markdown_path = "./knowledgebase/"
+        markdown_loader = DirectoryLoader(markdown_path, glob='./*.md', loader_cls=UnstructuredMarkdownLoader)
+        markdown_docs = markdown_loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=30)
+        docs = text_splitter.split_documents(markdown_docs)
+        vector_db= Chroma.from_documents(documents=docs,embedding=embeddings,persist_directory="./chroma_db")
+    else:
+        vector_db = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+
     return vector_db
 
 async def main():
@@ -54,14 +61,11 @@ async def main():
 
     new_message = {"role": "assistant", "content": ""}
     
-    print()
-    # next_input = input("> ")
-    print("> ")
     if my_prompt := st.chat_input("Escribe tu mensaje..."):
         st.chat_message("user").markdown(my_prompt)
         st.session_state.history.append({"role": "user", "content": my_prompt})
 
-        print(my_prompt)
+        #print(my_prompt)
         
         search_results = vector_db.similarity_search(my_prompt, k=3)
         some_context = ""
@@ -69,21 +73,24 @@ async def main():
             some_context += result.page_content + "\n\n"    
         
         prompt_value = template_1.format(context=some_context,question=my_prompt)
+        start = time.time()
         completion = await client.chat.completions.create(
         model="local-model",
         messages=[{"role": "user", "content": prompt_value}],
         temperature=0.7,
+        stream_options={"include_usage": True},
         stream=True,
         )
-        new_message = {"role": "assistant", "content": ""}
+        new_message = {"role": "assistant", "content": ""}  
         async for chunk in completion:
             if chunk.choices[0].delta.content:
-                print(chunk.choices[0].delta.content, end="")
-                respuesta=chunk.choices[0].delta.content
                 new_message["content"] += chunk.choices[0].delta.content 
         if new_message["content"]!="":   
+            end = time.time()
+            elapsed=end-start
             st.chat_message("assistant").markdown(new_message["content"])
             st.session_state.history.append({"role": "assistant", "content": new_message["content"]})
+            st.write(str(round(elapsed,1)) + " s. " + str(round(len(new_message["content"])/elapsed,0)) + " char/s")
 
 if __name__ == "__main__":
     asyncio.run(main())
