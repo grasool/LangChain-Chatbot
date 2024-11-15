8a3b2e89fd26f18ed4b26b5b9b55977842bbb6f0
P82maarj <55245863+P82maarj@users.noreply.github.com>
2024-11-16 00:00:10 +0100

    -

--- RAG-chatbot.py
+++ RAG-chatbot.py
@@ -5,13 +5,11 @@ import asyncio
 import os
 import time
 from openai import AsyncOpenAI
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.document_loaders import DirectoryLoader, UnstructuredFileLoader, UnstructuredMarkdownLoader
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.document_loaders import DirectoryLoader, UnstructuredMarkdownLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains import LLMChain
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
 
 
